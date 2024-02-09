91e94db4120210834e23e1001210119147f649ef
Mumbi69 <winfriendmumbi69@gmail.com>
2024-02-09 03:10:54 +0300

    regexing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Regex-ing"""
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """function named filter_datum which takes four arguments"""
+    return(re.sub(rf"({'|'.join(fields)})=.*?{separator}",
+           rf"\1={redaction}{separator}", message))
