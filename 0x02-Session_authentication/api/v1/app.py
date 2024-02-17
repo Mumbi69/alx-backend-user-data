984c748d1cc0ebb7e634f98e07fd3a0bfa1c0e3a
Mumbi69 <winfriendmumbi69@gmail.com>
2024-02-17 17:25:10 +0300

    change

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,11 +31,11 @@ def before_request_func():
     if not auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
                                             '/api/v1/forbidden/']):
-        return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    return
 
 
 @app.errorhandler(404)
